1db381c092262d29bf1ef744fabc689a4b4272bb
Steven maulny <steven.maulny@gmail.com>
2025-12-07 13:07:01 +0100

    wip

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,39 @@
-output "gateway_info" {
-  description = "Informations complètes sur les gateways créées"
-  value = {
-    for k, gw in ovh_cloud_project_gateway.gateway :
-    k => {
-      id            = gw.id
-      name          = gw.name
-      region        = gw.region
-      model         = gw.model
-      subnet_id     = gw.subnet_id
-      network_id    = gw.network_id
-    }
-  }
+output "id" {
+  description = "ID of the created gateway."
+  value       = ovh_cloud_project_gateway.this.id
+}
+
+output "service_name" {
+  description = "OVH service name linked to the gateway."
+  value       = ovh_cloud_project_gateway.this.service_name
+}
+
+output "name" {
+  description = "Name of the gateway."
+  value       = ovh_cloud_project_gateway.this.name
+}
+
+output "region" {
+  description = "Region where the gateway is deployed."
+  value       = ovh_cloud_project_gateway.this.region
+}
+
+output "model" {
+  description = "Gateway model."
+  value       = ovh_cloud_project_gateway.this.model
+}
+
+output "network_id" {
+  description = "Network ID attached to the gateway."
+  value       = ovh_cloud_project_gateway.this.network_id
+}
+
+output "subnet_id" {
+  description = "Subnet ID attached to the gateway."
+  value       = ovh_cloud_project_gateway.this.subnet_id
+}
+
+output "status" {
+  description = "Status of the gateway."
+  value       = ovh_cloud_project_gateway.this.status
 }
